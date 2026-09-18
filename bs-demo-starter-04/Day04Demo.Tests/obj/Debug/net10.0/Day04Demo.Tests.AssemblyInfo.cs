db336d513ae11fb589f5a6966b411b23b4aea192
Sean Paulson <auggle@hotmail.com>
2026-09-18 08:04:53 -0600

    lesson 4: reducks

--- bs-demo-starter-04/Day04Demo.Tests/obj/Debug/net10.0/Day04Demo.Tests.AssemblyInfo.cs
+++ bs-demo-starter-04/Day04Demo.Tests/obj/Debug/net10.0/Day04Demo.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day04Demo.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f60401de7a6b7b2b1fb20076a7d5bb7955070fbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day04Demo.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day04Demo.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
